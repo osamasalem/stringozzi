aea606f6054d2976d2a4dd4542d8154064ace679
Osama Salem <osama__salem@hotmail.com>
2020-10-31 15:29:13 +0200

    Another fix for travis

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ export CXXFLAGS=-m32
 mkdir -p build_Linux_DEBUG_x86 
 cd build_Linux_DEBUG_x86
 cmake -DCMAKE_BUILD_TYPE=Debug -DARCH=x86 -Dgtest_force_shared_crt=TRUE -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON ..
-cmake --build .
+cmake --build . --config Debug
 cd ..
 
 
@@ -13,21 +13,22 @@ export CXXFLAGS=-m32
 mkdir -p build_Linux_RELEASE_x86 
 cd build_Linux_RELEASE_x86
 cmake -DCMAKE_BUILD_TYPE=Release -DARCH=x86 -Dgtest_force_shared_crt=TRUE -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON ..
-cmake --build .
+cmake --build . --config Release
 cd ..
 
 export CXXFLAGS=-m64
 mkdir -p build_Linux_DEBUG_x64 
 cd build_Linux_DEBUG_x64
 cmake -DCMAKE_BUILD_TYPE=Debug -DARCH=x64 -Dgtest_force_shared_crt=TRUE -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON ..
-cmake --build .
+cmake --build . --config Debug
 cd ..
 
 export CXXFLAGS=-m64
 mkdir -p build_Linux_RELEASE_x64 
 cd build_Linux_RELEASE_x64
 cmake -DCMAKE_BUILD_TYPE=Release -DARCH=x64 -Dgtest_force_shared_crt=TRUE -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON ..
-cmake --build .
+cmake --build . --config Release
 cd ..
 
-../bin_Linux/stringozzi.test
+
+bin.tmp/Linux/Debug/x86/stringozzi.test
